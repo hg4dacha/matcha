565110d66d4a764f6633ce173a80ff69f370ff32
hgadacha <hgadacha@student.42.fr>
2022-03-27 18:01:42 +0200

    matcha

---
+++ api/test.php
@@ -0,0 +1,28 @@
+<?php
+
+// $curl = curl_init();
+
+// curl_setopt_array($curl, [
+//   CURLOPT_URL => "https://api.iconfinder.com/v4/icons/search?query=arrow&count=10",
+//   CURLOPT_RETURNTRANSFER => true,
+//   CURLOPT_ENCODING => "",
+//   CURLOPT_MAXREDIRS => 10,
+//   CURLOPT_TIMEOUT => 30,
+//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//   CURLOPT_CUSTOMREQUEST => "GET",
+//   CURLOPT_HTTPHEADER => [
+//     "Accept: application/json",
+//     "Authorization: Bearer X0vjEUN6KRlxbp2DoUkyHeM0VOmxY91rA6BbU5j3Xu6wDodwS0McmilLPBWDUcJ1"
+//   ],
+// ]);
+
+// $response = curl_exec($curl);
+// $err = curl_error($curl);
+
+// curl_close($curl);
+
+// if ($err) {
+//   echo "cURL Error #:" . $err;
+// } else {
+//   echo $response;
+// }
