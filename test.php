74a49572cb8ac9eb413eac6ab02c2853b2d0571a
hgadacha <hgadacha@student.42.fr>
2022-06-30 11:25:33 +0200

    Deleting the test.php file

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// https://randomuser.me/api/?results=500&nat=fr&gender=male&inc=name,email&exc=gender,login,registered,dob,phone,cell,id,picture,nat,location
-// https://geo.api.gouv.fr/departements/93/communes?fields=code,nom,region,centre
-// https://avatars.dicebear.com/api/avataaars/3154323123.svg?top=shortHair&hatColor=black&hairColor=black&facialHair=majestic&facialHairChance=100&facialHairColor=black&skin=light&accessories=prescription02&accessoriesColor=black&accessoriesChance=100&eyes=default
-
-
-
-?>
